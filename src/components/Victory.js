3b46eb75a50797fc55bbf3bd0b9629deee2cb56b
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2022-05-27 11:39:34 -0400

    initial commit

---
+++ src/components/Victory.js
@@ -0,0 +1,45 @@
+class App extends React.Component {
+  render() {
+    return (
+      <VictoryChart
+        domainPadding={20}
+        theme={VictoryTheme.material}
+      >
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x) => (`$${x / 1000}k`)}
+        />
+        <VictoryStack
+          colorScale={"warm"}
+        >
+          <VictoryBar
+            data={data2012}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2013}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2014}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2015}
+            x="quarter"
+            y="earnings"
+          />
+        </VictoryStack>
+      </VictoryChart>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, mountNode);
